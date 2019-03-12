e3bbfe29cdb13837c931545fa78bfdde78c7a6b4
houssemshili <48476266+houssemshili@users.noreply.github.com>
2019-03-12 20:37:12 +0100

    Add files via upload

---
+++ WebApplication3/App_Start/BundleConfig.cs
@@ -0,0 +1,18 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace WebApplication3
+{
+	public class BundleConfig
+	{
+		// Pour plus d'informations sur le regroupement, visitez https://go.microsoft.com/fwlink/?LinkId=301862
+		public static void RegisterBundles(BundleCollection bundles)
+		{
+			bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+					  "~/Content/js/*.js"));
+
+			bundles.Add(new StyleBundle("~/Content/css").Include(
+					  "~/Content/*.css"));
+		}
+	}
+}
